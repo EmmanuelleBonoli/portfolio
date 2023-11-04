e7c3ce97f7e8bb788c5ac5164e553787db10e728
Emmanuelle BONOLI <manu.bonoli@gmail.com>
2023-11-04 23:19:06 +0100

    maj site portfolio correction home et interests

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,11 @@ const Home = () => {
       <div className="background" />
       <div className="presentationDev">
         <h1>
-          Bienvenue sur mon site internet, Emmanuelle BONOLI, web développeuse
+          Bienvenue sur mon site internet ! Je me présente : Emmanuelle BONOLI, web développeuse
           en devenir !
         </h1>
         <p>
-          {`Ce dernier a vocation à vous présenter les différents projets que j'ai réalisé mais également mon parcours et les moyens de me contacter. Alors bonne visite !`}
+          {`Ce dernier a vocation à vous présenter les différents projets que j'ai réalisés, mais également mon parcours et les moyens de me contacter. Alors bonne visite !`}
         </p>
       </div>
     </div>
